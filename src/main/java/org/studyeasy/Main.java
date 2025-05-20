949caf0b309c39dad457772a12180d5e4d90a0b6
mohdswar <mohdswar9@gmail.com>
2025-05-20 15:47:18 +0300

    fuckingg

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -3,7 +3,7 @@ package org.studyeasy;
 public class Main {
     public static void main(String[] args) {
 int a =1;
-    System.out.printf("Hello and welcome!");
+    System.out.printf("Helllllllo and welcome!");
         System.out.print(a);
     }
 }
